f20e695d9b8893aa654538b29e9a32f5cab35ac9
GitHub Action <action@github.com>
2022-08-30 12:05:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/smartniu/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/smartniu/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.smartniu.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.smartniu.testplugin.MyBundle
 
 class MyApplicationService {
 
